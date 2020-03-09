c1c2d4ecf45047c43c38a416d11c6e669c1b6414
Garrett Johnson <dgj918@gmail.com>
2020-03-09 18:41:13 -0500

    basic structure

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProjectsComponent } from './apps/project-tracker/projects/projects.component';
+import { KanbanBoardComponent } from './apps/project-tracker/kanban-board/kanban-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    AuthComponent,
+    ProjectsComponent,
+    KanbanBoardComponent
   ],
   imports: [
     BrowserModule,
