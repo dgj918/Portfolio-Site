e6c9e6e106585b71d54aab5c8624c5e090850916
Garrett Johnson <dgj918@gmail.com>
2020-04-04 10:19:14 -0500

    clean up

--- portfolio-site/src/app/apps/headache-tracker/headache-tracker.component.ts
+++ portfolio-site/src/app/apps/headache-tracker/headache-tracker.component.ts
@@ -10,22 +10,6 @@ export interface HeadacheType {
   medicine: string;
 }
 
-const ELEMENT_DATA: HeadacheType[] = [
-  {date: 'Jan 1', intensity: 6, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 2', intensity: 7, trigger: 'Wine', medicine: 'IB Profin'},
-  {date: 'Jan 3', intensity: 4, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 4', intensity: 3, trigger: 'Night Shift', medicine: 'IB Profin'},
-  {date: 'Jan 5', intensity: 5, trigger: 'Workout', medicine: 'IB Profin'},
-  {date: 'Jan 6', intensity: 6, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 7', intensity: 8, trigger: 'Night Shift', medicine: 'IB Profin'},
-  {date: 'Jan 8', intensity: 3, trigger: 'Wine', medicine: 'IB Profin'},
-  {date: 'Jan 9', intensity: 2, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 10', intensity: 5, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 11', intensity: 5, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 12', intensity: 5, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 13', intensity: 5, trigger: 'Call', medicine: 'IB Profin'},
-  {date: 'Jan 14', intensity: 5, trigger: 'Call', medicine: 'IB Profin'},
-];
 @Component({
   selector: 'app-headache-tracker',
   templateUrl: './headache-tracker.component.html',
@@ -52,7 +36,6 @@ export class HeadacheTrackerComponent implements OnInit {
       })
     )
     .subscribe((data) => {
-      console.log(data)
       let headacheData: any = data
       this.dataSource = headacheData
     })
@@ -73,7 +56,6 @@ export class HeadacheTrackerComponent implements OnInit {
       })
     )
     .subscribe((data) => {
-      console.log(data)
       let headacheData: any = data
       this.dataSource = headacheData
     })
